07e50010cca15bbe740c2246f55c3dfb2486ba93
Nothosaurs <597057969@qq.com>
2019-08-22 09:10:07 +0800

    .

--- js/base.js
+++ js/base.js
@@ -1,21 +1,23 @@
 // 判断函数类型方法 es6的写法
 const isType = type => target => {
-    let stringType = Object.prototype.toString.call(target);
-    return `[Object ${type}]` === stringType;
+    let stringType = Object.prototype.toString.call(target)
+    return `[object ${type}]` === stringType
 }
 // es5的写法
-const isType1 = function(type){
-    return function(target){
-        let stringType = Object.prototype.toString.call(target);
-        return `[Object ${type}]` === stringType;
-    } 
+const isType1 = function (type) {
+    return function (target1) {
+        let stringType = Object.prototype.toString.call(target1);
+        return `[object ${type}]` === stringType;
+    }
 }
+// 正则匹配，返回真正的类型值
+const isType2 = type => (/^\[object\s(.*)\]$/.exec(Object.prototype.toString.call(type)))[1];
 
 // 判断数组
 const isArray = isType('Array');
-// 判断NaN
-const isNaN = isType1('NaN');
+// 判断null
+const isNull = isType1('Null');
 
-console.log(isArray([]),isNaN(NaN));
+console.log(isArray([]), isNull(null),isNaN(NaN),isType2(''));
 
 
